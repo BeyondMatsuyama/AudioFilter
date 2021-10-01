cb644116b37fad8c4d6a2ac4c437a24d242c0566
BeyondMatsuyama <55428800+BeyondMatsuyama@users.noreply.github.com>
2021-10-01 18:34:25 +0900

    Add files via upload
    first commit

---
+++ Assets/Scripts/AudioTester.cs
@@ -0,0 +1,199 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioTester : MonoBehaviour
+{
+    enum FilterNo
+    {
+        Chorus = 0,
+        Distortion,
+        Echo,
+        Reverv,
+        Num
+    }
+
+    private const float LowHz       = 22000f;
+    private const float HighHz      = 10f;
+    private const float InitVolume  = 0.4f;
+    private const float InitPitch   = 1f;
+
+
+    [SerializeField] AudioSource seSource;
+    [SerializeField] AudioSource bgmSource;
+    [SerializeField] AudioSource voiceSource;
+
+    [SerializeField] Slider sldVolume;
+    [SerializeField] Text   txtVolume;
+    [SerializeField] Slider sldPitch;
+    [SerializeField] Text   txtPitch;
+    [SerializeField] Slider sldLoPass;
+    [SerializeField] Text   txtLoPass;
+    [SerializeField] Slider sldHiPass;
+    [SerializeField] Text   txtHiPass;
+
+    [SerializeField] Toggle tglChorus;
+    [SerializeField] Toggle tglDistotion;
+    [SerializeField] Toggle tglEcho;
+    [SerializeField] Toggle tglReverv;
+    [SerializeField] Toggle tglLoop;
+
+    [SerializeField] Button btnSePlay;
+    [SerializeField] Button btnBgmPlay;
+    [SerializeField] Button btnVoicePlay;
+    [SerializeField] Button btnAllStop;
+
+    [SerializeField] AudioChorusFilter      fltChorus;
+    [SerializeField] AudioDistortionFilter  fltDistortion;
+    [SerializeField] AudioEchoFilter        fltEcho;
+    [SerializeField] AudioReverbFilter      fltReverb;
+    [SerializeField] AudioLowPassFilter     fltLoPass;
+    [SerializeField] AudioHighPassFilter    fltHiPass;
+
+    void Awake()
+    {
+        // フィルタ設定
+        for (int i = 0; i < (int)FilterNo.Num; i++)
+        {
+            OnClickToggleFilter(i);
+        }
+
+        // サウンド設定
+        setVolume(InitVolume);  // 音量設定
+        setPitch(InitPitch);    // ピッチ設定
+        setLoPass(1f);          // 低域通過
+        setHiPass(0f);          // 高域通過
+
+        OnClickToggleSeLoop();  // SE ループ
+    }
+
+    private void Start()
+    {
+        // Voice 再生・停止
+        btnVoicePlay.onClick.AddListener(() =>
+        {
+            if (!voiceSource.isPlaying) voiceSource.Play();
+            else voiceSource.Stop();
+        });
+        // SE 再生・停止
+        btnSePlay.onClick.AddListener(() =>
+        {
+            if (!seSource.isPlaying) seSource.Play();
+            else seSource.Stop();
+        });
+        // BGM 再生・停止
+        btnBgmPlay.onClick.AddListener(() =>
+        {
+            if (!bgmSource.isPlaying) bgmSource.Play();
+            else bgmSource.Stop();
+        });
+        // 全停止
+        btnAllStop.onClick.AddListener(() =>
+        {
+            if (voiceSource.isPlaying) voiceSource.Stop();
+            if (seSource.isPlaying) seSource.Stop();
+            if (bgmSource.isPlaying) bgmSource.Stop();
+        });
+
+        // ボリューム
+        sldVolume.onValueChanged.AddListener((value) =>
+        {
+            setVolume(value);
+        });
+        // ピッチ
+        sldPitch.onValueChanged.AddListener((value) =>
+        {
+            setPitch(value);
+        });
+        // 低音通過
+        sldLoPass.onValueChanged.AddListener((value) =>
+        {
+            setLoPass(value);
+        });
+        // 高音通過
+        sldHiPass.onValueChanged.AddListener((value) =>
+        {
+            setHiPass(value);
+        });
+    }
+
+
+    // 音量設定
+    private void setVolume(float val)
+    {
+        seSource.volume  = val;
+        bgmSource.volume = val;
+        txtVolume.text = string.Format("{0:0.##}", val);
+    }
+    // ピッチ設定
+    private void setPitch(float val)
+    {
+        seSource.pitch  = val;
+        bgmSource.pitch = val;
+        txtPitch.text = string.Format("{0:0.##}", val);
+    }
+
+    // 低域通過フィルター設定
+    private void setLoPass(float val)
+    {
+        var loPassHz = ExpHz(val);
+        fltLoPass.cutoffFrequency = loPassHz;
+        txtLoPass.text = string.Format("{0:#}", loPassHz);
+    }
+
+    // 高域通過フィルター設定
+    private void setHiPass(float val)
+    {
+        var hiPassHz = ExpHz(val);
+        fltHiPass.cutoffFrequency = hiPassHz;
+        txtHiPass.text = string.Format("{0:#}", hiPassHz);
+    }
+
+    // Filter 用トグル
+    public void OnClickToggleFilter(int no)
+    {
+        // Debug.Log("Click Toggle : " + no);
+        switch ((FilterNo)no)
+        {
+            case FilterNo.Chorus:
+                fltChorus.enabled = tglChorus.isOn;
+                break;
+            case FilterNo.Distortion:
+                fltDistortion.enabled = tglDistotion.isOn;
+                break;
+            case FilterNo.Echo:
+                fltEcho.enabled = tglEcho.isOn;
+                break;
+            case FilterNo.Reverv:
+                fltReverb.enabled = tglReverv.isOn;
+                break;
+        }
+    }
+
+    // SE ループ用トグル
+    public void OnClickToggleSeLoop()
+    {
+        seSource.loop = tglLoop.isOn;
+    }
+
+    // 10hzで0、22000hzで1になるような対数変換を返す
+    float LogHz(float hz)
+    {
+        return (Mathf.Log(hz) - Mathf.Log(HighHz)) / (Mathf.Log(LowHz) - Mathf.Log(HighHz));
+    }
+
+    // 上の逆変換
+    // logHz = (log(hz) - log(10)) / (log(22000) - log(10))
+    // logHz * (log(22000) - log(10)) = log(hz) - log(10)
+    // log(hz) = (logHz * (log(22000) - log(10))) + log(10)
+    // expして
+    // hz = Exp((logHz * (log(22000) - log(10))) + log(10))
+    //    = Exp(logHz * (log(22000) - log(10))) * 10
+    float ExpHz(float logHz)
+    {
+        var t = Mathf.Log(LowHz) - Mathf.Log(HighHz);
+        return Mathf.Exp(logHz * t) * 10f;
+    }
+
+}
---
+++ Assets/Scripts/SpectrumViewer.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectrumViewer : MonoBehaviour
+{
+    private const int SampleCount = 8192;
+    private const int BinCount = 512;
+    private const float BarWidth = 3f;
+
+    // private const int graphCenterFrequency = 1000;
+    private const int graphMinFrequency = 20;
+    private const int graphMaxFrequency = 20000;
+
+    [SerializeField] private Transform parent;
+    [SerializeField] private GameObject objBar;
+    private List<RectTransform> spectrumBars = new List<RectTransform>();
+    private float[] spectrumData = new float[SampleCount];
+    private float[] bins = new float[BinCount];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 初期化
+        createSpectrumBars();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 描画
+        draw();
+    }
+
+    // スペクトラムアナライザ的な何か
+    private void createSpectrumBars()
+    {
+        Vector3 pos = Vector3.zero;
+        for (int i = 0; i < BinCount; i++)
+        {
+            var obj = GameObject.Instantiate(objBar);
+            Transform t = obj.transform;
+            t.SetParent(parent);
+            t.localScale = Vector3.one;
+            t.name = "sp_bar_" + i;
+
+            t.localPosition = pos;
+            pos.x += BarWidth;
+
+            spectrumBars.Add(t as RectTransform);
+        }
+    }
+
+    private void draw()
+    {
+        AudioListener.GetSpectrumData(spectrumData, 0, FFTWindow.Hanning);
+        var outputF = AudioSettings.outputSampleRate;
+        // 全binを初期化
+        for (int i=0; i<bins.Length; i++)
+        {
+            bins[i] = 0f;
+        }
+
+        var logMaxF = Mathf.Log(graphMaxFrequency); // 上のbinの周波数のlog
+        var logMinF = Mathf.Log(graphMinFrequency);
+        var logRange = logMaxF - logMinF;
+        if (logRange <= 0f)
+        {
+            logRange = 8f;
+        }
+        // まず周波数分類
+        for (int i=0; i<spectrumData.Length; i++)
+        {
+            var f = outputF * 0.5f * (float)i / (float)spectrumData.Length;
+            if (f == 0f)
+            {
+                f = float.Epsilon;
+            }
+            // 対数を取ってどのビンに入るか確定
+            float binValue = (float)bins.Length * (Mathf.Log(f) - logMinF) / logRange;
+            int binIndex = Mathf.FloorToInt(binValue);
+            if ((binIndex >= 0) && (binIndex < bins.Length))
+            {
+                // そのビンにデータを加算
+                bins[binIndex] += spectrumData[i];
+            }
+        }
+        // バーに反映
+        for(int i=0; i<BinCount; i++)
+        {
+            float v = bins[i];
+            float y = Mathf.Min(v * 8000f, 420f);
+            Vector2 sd = new Vector2(BarWidth, y);
+            spectrumBars[i].sizeDelta = sd;
+        }
+    }
+
+}
